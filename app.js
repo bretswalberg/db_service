36e75c1a70dbca44dc37a5f0eeb9a8dc91435e69
Bret Swalberg <swalberb@oregonstate.edu>
2022-02-10 22:31:40 -0700

    Generic API up and running

---
+++ app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var db = require('./dbcon.js');
+var portnum = 8123		
+app.set('port', portnum);
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/*-------------------------
+  API ENDPOINTS
+-------------------------*/
+app.get('/get_something', function(req, res, next) {
+    res.send('API call successful');
+
+    /*
+	var sql = "SELECT * " + 
+			" FROM someTable " + 
+			" WHERE ID = ? "
+    params = [req.query.id]
+
+	db.run(sql, params, (err, rows) => {
+        if (err) {
+            next(err);
+            return;
+        }
+        res.send(rows)
+    });
+    */
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
